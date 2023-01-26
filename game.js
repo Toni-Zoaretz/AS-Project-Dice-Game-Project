35331f6aa4be4c1f3a148543688b6f216318e0ca
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-01-26 17:20:56 +0200

    1

--- game.js
+++ game.js
@@ -1,1 +1,7 @@
-console.log("test");
+const player1total = document.querySelector("#player-1-total");
+const player2total = document.querySelector("#player-2-total");
+const player1current = document.querySelector("#player-1-current");
+const player2current = document.querySelector("#player-2-current");
+const newGame = document.querySelector("#new-game-btn");
+const rollDice = document.querySelector("#roll-dice-btn");
+const hold = document.querySelector("#hold");
